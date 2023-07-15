5fcc798169b92b4c04476a2d9c77faf5e2f68454
branmeco <branmeco17@gmail.com>
2023-07-15 15:05:42 -0500

    Habilitando Handlebars como Template Engine

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = require('./routes');
+const exphbs = require('express-handlebars');
+const path = require('./routes');
 
 const app = express();
+
+//Habilitar handlebars como view
+app.engine('handlebars',
+    exphbs.engine({
+        defaultLayout: 'layout'
+    })
+)
+app.set('view engine', 'handlebars');
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', router());
 
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 module.exports = () => {
     router.get('/', (req, res) => {
         res.send('Funciona');
-    })
+    });
+
+    return router;
 }
