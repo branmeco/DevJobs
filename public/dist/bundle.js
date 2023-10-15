8659a81131a453e0920e44233a940de0184c15cb
branmeco <branmeco17@gmail.com>
2023-10-14 22:27:36 -0500

    Almacenando la vacante en la base de datos

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -15,7 +15,7 @@
   \**************************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://devjobs/./public/js/app.js?");
+eval("document.addEventListener('DOMContentLoaded', () => {\n    const skills = document.querySelector('.lista-conocimientos');\n\n    if (skills) {\n        skills.addEventListener('click', agregarSkills);\n    }\n})\n\nconst skills = new Set();\n\nconst agregarSkills = (e) => {\n    if (e.target.tagName === 'LI') {\n        if (e.target.classList.contains('activo')) {\n            //Quitar del set y quitar la clase\n            skills.delete(e.target.textContent);\n            e.target.classList.remove('activo');\n        } else {\n            //Agregar del set y agregar la clase\n            skills.add(e.target.textContent);\n            e.target.classList.add('activo');\n        }\n    }\n\n    const skillsArray = [...skills]\n    document.querySelector('#skills').value = skillsArray;\n}\n\n//# sourceURL=webpack://devjobs/./public/js/app.js?");
 
 /***/ })
 
